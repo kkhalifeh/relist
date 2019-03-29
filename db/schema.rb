f4af5f6cbb1ba660ff0c4ec72891e9cf588741f4
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-03-29 10:20:54 -0400

    removed categories model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_29_141534) do
+ActiveRecord::Schema.define(version: 2019_03_29_141933) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,12 +31,6 @@ ActiveRecord::Schema.define(version: 2019_03_29_141534) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "label"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "listings", force: :cascade do |t|
